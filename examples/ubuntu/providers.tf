91346e044a6cdcd02f30157ca60211ff39436f68
Marco Franssen <marco.franssen@philips.com>
2021-03-10 15:33:04 +0100

    Update ubuntu example with assume_role comment

--- examples/ubuntu/providers.tf
+++ examples/ubuntu/providers.tf
@@ -1,4 +1,9 @@
 provider "aws" {
   region  = local.aws_region
   version = "3.20"
+
+  // If you use roles with specific permissons please add your role
+  // assume_role {
+  //   role_arn = "arn:aws:iam::123456789012:role/MyAdminRole"
+  // }
 }
